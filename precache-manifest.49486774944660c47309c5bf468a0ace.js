55a18f6a0ab9095ffe63549e1fd2a5d1fee7fb9b
Eugene Gnedenko <deadup4@gmail.com>
2021-08-31 13:08:40 +0300

    Updates

---
+++ precache-manifest.49486774944660c47309c5bf468a0ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac20cbd994d710ee4cb18ecdf8a88bdf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d78444f400b5772d9f",
+    "url": "./static/js/main.90e690c6.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.49486774944660c47309c5bf468a0ace.js"
 );
 
 self.addEventListener('message', (event) => {
